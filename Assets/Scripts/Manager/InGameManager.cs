9589e359a30a27b337393d656ed51b719bacb10f
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2023-04-26 07:48:36 +0900

    feat：ゲーム全体を管理するクラスを追加

--- Assets/Scripts/Manager/InGameManager.cs
+++ Assets/Scripts/Manager/InGameManager.cs
@@ -1,7 +1,34 @@
+using Commons.Enum;
 using UnityEngine;
+
 
 public class InGameManager : MonoBehaviour
 {
-    [SerializeField] private MainUI _ui;
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField] private InGameMoveCommandManager _commandManager;
 
+    [SerializeField] private StageManager _stageCreateManager;
+    
+    
+    //TODO:単純に初期化で色々な処理をしているので、細かく分ける
+    private void Start()
+    {
+        _stageCreateManager.LoadTileData();
+        _stageCreateManager.CreateStage();
+        
+        _commandManager.Initialize();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void OnStageChanged(InGameEnum.State state)
+    {
+        switch (state)
+        {
+            
+        }
+    }
 }

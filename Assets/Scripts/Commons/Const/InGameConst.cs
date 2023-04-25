7bcc9a1a82f5e74c544780f5ca3f363f320837c9
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2023-04-26 07:49:06 +0900

    feat：ゲーム全体で使われる固定値

--- Assets/Scripts/Commons/Const/InGameConst.cs
+++ Assets/Scripts/Commons/Const/InGameConst.cs
@@ -2,6 +2,30 @@
 {
     public class InGameConst
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string PlayerObjectTag="Player";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string TileName="tile";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string TargetName = "target";
         
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string PlayerName="player";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static string BlockName="block";
     }
 }

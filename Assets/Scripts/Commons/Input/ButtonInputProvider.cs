5112eb844f8b597707395a54c60edc44c9adf5d1
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2023-04-28 16:46:50 +0900

    refactor：入力をスマートフォン対応した

--- Assets/Scripts/Commons/Input/ButtonInputProvider.cs
+++ Assets/Scripts/Commons/Input/ButtonInputProvider.cs
@@ -1,41 +1,43 @@
 ﻿using UnityEngine;
 using UniRx;
+using UniRx.Triggers;
 using UnityEngine.UI;
 using Zenject;
 
 namespace Commons.Input
 {
-    public class ButtonInputProvider : IInputEventProvider , IInitializable
+    public class ButtonInputProvider : IInputEventProvider, IInitializable
     {
         /// <summary>
-        /// 
+        /// 前方ボタン
         /// </summary>
         private Button _aheadButton;
-        
+
         /// <summary>
-        /// 
+        /// 左向ボタン
         /// </summary>
         private Button _leftButton;
-        
+
         /// <summary>
-        /// 
+        /// 右向ボタン
         /// </summary>
         private Button _rightButton;
-        
+
         /// <summary>
-        /// 
+        /// 後方ボタン
         /// </summary>
         private Button _backButton;
-        
+
         /// <summary>
-        /// 
+        /// Undoボタン
         /// </summary>
         private Button _undoButton;
 
         /// <summary>
-        /// 
+        /// コンストラクタ
         /// </summary>
-        public ButtonInputProvider(Button aheadButton,Button leftButton,Button rightButton,Button backButton,Button undoButton)
+        public ButtonInputProvider(Button aheadButton, Button leftButton, Button rightButton, Button backButton,
+            Button undoButton)
         {
             _aheadButton = aheadButton;
             _leftButton = leftButton;
@@ -43,25 +45,40 @@ namespace Commons.Input
             _backButton = backButton;
             _undoButton = undoButton;
         }
-        
+
         /// <summary>
-        /// 
+        /// 進行方向
         /// </summary>
         public IReadOnlyReactiveProperty<Vector3> MoveDirection => _moveDirection;
         private ReactiveProperty<Vector3> _moveDirection = new ReactiveProperty<Vector3>();
 
         /// <summary>
-        /// 
+        /// Undoボタンが押されたか
         /// </summary>
         public IReadOnlyReactiveProperty<bool> UndoButton => _undo;
         private ReactiveProperty<bool> _undo = new ReactiveProperty<bool>();
 
         /// <summary>
-        /// 
+        /// 初期化
         /// </summary>
         public void Initialize()
         {
-           
+            //キーが押されたら、進行方向を設定する
+            Observable.Merge(
+                    _aheadButton.OnClickAsObservable().Select(_ => Vector3.forward),
+                    _rightButton.OnClickAsObservable().Select(_ => Vector3.right),
+                    _backButton.OnClickAsObservable().Select(_ => Vector3.back),
+                    _leftButton.OnClickAsObservable().Select(_ => Vector3.left)
+                )
+                .Subscribe(value => _moveDirection.SetValueAndForceNotify(value));
+
+            //キーが押されたら、フラグを設定する
+            Observable.Merge(
+                    _undoButton.OnPointerDownAsObservable().Select(_ => true),
+                    _undoButton.OnPointerUpAsObservable().Select(_ => false)
+                )
+                .DistinctUntilChanged()
+                .Subscribe(value => _undo.Value = value);
         }
     }
 }
--- Assets/Scripts/Commons/Input/InputEventInstaller.cs
+++ Assets/Scripts/Commons/Input/InputEventInstaller.cs
@@ -1,43 +1,45 @@
-using Commons.Input;
 using UnityEngine;
 using Zenject;
 using UnityEngine.UI;
 
-public class InputEventInstaller : MonoInstaller
+namespace Commons.Input
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    [SerializeField] private Button _aheadButton;
-
-    /// <summary>
-    /// 
-    /// </summary>
-    [SerializeField] private Button _leftButton;
-
-    /// <summary>
-    /// 
-    /// </summary>
-    [SerializeField] private Button _rightButton;
-
-    /// <summary>
-    /// 
-    /// </summary>
-    [SerializeField] private Button _backButton;
-
-    /// <summary>
-    /// 
-    /// </summary>
-    [SerializeField] private Button _undoButton;
-
-    public override void InstallBindings()
+    public class InputEventInstaller : MonoInstaller
     {
+        /// <summary>
+        /// 前方のボタン
+        /// </summary>
+        [SerializeField] private Button _aheadButton;
+
+        /// <summary>
+        /// 左向のボタン
+        /// </summary>
+        [SerializeField] private Button _leftButton;
+
+        /// <summary>
+        /// 右向のボタン
+        /// </summary>
+        [SerializeField] private Button _rightButton;
+
+        /// <summary>
+        /// 後方のボタン
+        /// </summary>
+        [SerializeField] private Button _backButton;
+
+        /// <summary>
+        /// Undoボタン
+        /// </summary>
+        [SerializeField] private Button _undoButton;
+
+        public override void InstallBindings()
+        {
 #if UNITY_EDITOR
-        Container.Bind(typeof(IInputEventProvider), typeof(IInitializable)).To<KeyInputProvider>()
-            .AsCached();
+            Container.Bind(typeof(IInputEventProvider), typeof(IInitializable)).To<KeyInputProvider>()
+                .AsCached();
 #elif UNITY_ANDROID
         Container.Bind(typeof(IInputEventProvider),typeof(IInitializable)).To<ButtonInputProvider>()
             .AsCached().WithArguments(_aheadButton,_leftButton,_rightButton,_backButton,_undoButton);
 #endif
+        }
     }
 }

74ec44a813fbcae62ac16ea2d828db6897daf5c6
saitocastle1900_test <s2023178@cco.kanagawa-it.ac.jp>
2023-05-01 18:24:44 +0900

    feat：UIの描画順を変更

--- Assets/Scripts/Manager/InGameManager.cs
+++ Assets/Scripts/Manager/InGameManager.cs
@@ -1,9 +1,12 @@
 using Manager.Command;
+using Manager.Stage;
 using Manager.State;
 using UI.InGame;
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Zenject;
 
 namespace Manager
 {
@@ -18,17 +21,22 @@ namespace Manager
         /// <summary>
         /// InGameMoveCommandManager
         /// </summary>
-        [SerializeField] private InGameMoveCommandManager _commandManager;
+        [SerializeField] private InGameCommandManager _commandManager;
 
         /// <summary>
         /// StageManager
         /// </summary>
-        [SerializeField] private StageManager _stageManager;
+        [SerializeField] private StageGenerator _stageManager;
 
         /// <summary>
         /// InGameUIController
         /// </summary>
         [SerializeField] private InGameUIController _uiController;
+
+        /// <summary>
+        /// IInputEventProvider
+        /// </summary>
+        [Inject] private IInputEventProvider _input;
 
         private void Start()
         {
@@ -36,6 +44,31 @@ namespace Manager
             _currentState = new InGameStateMachine(_commandManager, _stageManager, _uiController, this);
             _currentState.Initialize(_currentState.InitializingState);
             this.UpdateAsObservable().Subscribe(_ => _currentState.Update());
+
+            _input.IsReset.SkipLatestValueOnSubscribe().Subscribe(_=>Reset());
+            _input.IsQuit.SkipLatestValueOnSubscribe().Subscribe(_ => Quit());
+        }
+        
+        /// <summary>
+        /// リセット
+        /// </summary>
+        private void Reset()
+        {
+            SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+        }
+        
+        /// <summary>
+        /// ゲームを辞める
+        /// </summary>
+        private void Quit()
+        {
+#if (UNITY_EDITOR)
+            UnityEditor.EditorApplication.isPlaying = false;
+#elif (UNITY_ANDROID)
+    Application.Quit();
+#elif (UNITY_WEBGL)
+    Application.OpenURL("about:blank");
+#endif
         }
     }
 }
--- Assets/Scripts/UI/InGameUIController.cs
+++ Assets/Scripts/UI/InGameUIController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace UI.InGame
 {
@@ -8,18 +9,34 @@ namespace UI.InGame
         /// OperationButtons
         /// </summary>
         [SerializeField] private GameObject _operationButtons;
+        
+        /// <summary>
+        /// OperationPanels
+        /// </summary>
+        [SerializeField] private GameObject _operationPanels;
 
         /// <summary>
         /// StageNumber
         /// </summary>
         [SerializeField] private GameObject _stageNumber;
 
+        private void Start()
+        {
+            _operationPanels.SetActive(false);
+            _operationButtons.SetActive(false);
+            _stageNumber.gameObject.SetActive(false);
+        }
+
         /// <summary>
         /// 表示を設定
         /// </summary>
         public void SetView(bool isShow)
         {
+#if UNITY_EDITOR || UNITY_WEBGL
+            _operationPanels.SetActive(isShow);
+#elif UNITY_ANDROID
             _operationButtons.SetActive(isShow);
+#endif
             _stageNumber.gameObject.SetActive(isShow);
         }
     }
